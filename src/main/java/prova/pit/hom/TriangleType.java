138afadb7cb58c297275b0f8e7e7729f8c7350f7
Priscilla Raucci <priscilla.raucci@studenti.unimi.it>
2020-07-05 20:34:45 +0200

    cleanup

--- src/main/java/prova/pit/hom/TriangleType.java
+++
@@ -1,5 +0,0 @@
-package prova.pit.hom;
-
-public enum TriangleType {
-    INVALID, SCALENE, EQUILATERAL, ISOSCELES
-}
--- src/test/java/prova/pit/hom/MyClassTest.java
+++ src/test/java/prova/pit/hom/MyClassTest.java
@@ -1,24 +1,52 @@
 package prova.pit.hom;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.Assert.*;
 
 public class MyClassTest {
+    MyClass sut;
+
+    @BeforeEach
+    public void createSut(){
+        sut = new MyClass();
+    }
 
     @Test
-    public void testMe() {
-        //Yes, this should be multiple test cases...
-        MyClass sut = new MyClass();
+    public void testApositiveTrue1() {
         assertTrue(sut.myMethod(1, true));
+    }
+
+    @Test
+    public void testApositiveTrue2() {
         assertTrue(sut.myMethod(2, true));
+    }
 
+    @Test
+    public void testApositiveFalse1() {
         assertTrue(sut.myMethod(1, false));
+    }
+    @Test
+    public void testApositiveFalse2() {
         assertTrue(sut.myMethod(2, false));
+    }
 
+    @Test
+    public void testAzeroFalse(){
         assertFalse(sut.myMethod(0, false));
+    }
+    @Test
+    public void testAzeroTrue(){
         assertTrue(sut.myMethod(0, true));
+    }
 
+    @Test
+    public void testAnegativeTrue(){
         assertTrue(sut.myMethod(-2, true));
+    }
+    @Test
+    public void testAnegativeFalse(){
         assertFalse(sut.myMethod(-3, false));
     }
 }
